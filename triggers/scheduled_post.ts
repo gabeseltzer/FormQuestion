d9e51ae52575fddb3a5578c861a9a20f489e933c
Jesse Klimov <jesseklimov@gmail.com>
2023-07-25 21:08:33 -0400

    Scheduled post trigger

---
+++ triggers/scheduled_post.ts
@@ -0,0 +1,18 @@
+import { Trigger } from "deno-slack-api/types.ts";
+import { PostMessageList } from "../workflows/post_message_list.ts";
+import { TriggerTypes } from "deno-slack-api/mod.ts";
+
+const trigger: Trigger<typeof PostMessageList.definition> = {
+  type: TriggerTypes.Scheduled,
+  name: "Trigger a scheduled post_message_list",
+  workflow: `#/workflows/${workflowDef.definition.callback_id}`,
+  inputs: {},
+  schedule: {
+    // Schedule the post to happen once every day at 9 AM EST
+    //start_time: new Date(new Date().getTime() + 60000).toISOString(),
+    start_time: "2023-07-26T09:00:00Z",
+    frequency: { type: "daily", repeats_every: 1 },
+  },
+};
+
+export default trigger;
