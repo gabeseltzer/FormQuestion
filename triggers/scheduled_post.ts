55e6988b6d509b43f3e7324885f8be269951b2a5
Gabriel Seltzer <gabeseltzer@gmail.com>
2023-07-25 22:32:36 -0400

    fixing scheduled post

--- triggers/scheduled_post.ts
+++ triggers/scheduled_post.ts
@@ -1,18 +1,21 @@
 import { Trigger } from "deno-slack-api/types.ts";
-import { PostMessageList } from "../workflows/post_message_list.ts";
-import { TriggerTypes } from "deno-slack-api/mod.ts";
+import { TriggerContextData, TriggerTypes } from "deno-slack-api/mod.ts";
+import PostMessageList from "../workflows/post_message_list.ts";
 
 const trigger: Trigger<typeof PostMessageList.definition> = {
   type: TriggerTypes.Scheduled,
   name: "Trigger a scheduled post_message_list",
-  workflow: `#/workflows/${workflowDef.definition.callback_id}`,
+  workflow: "#/workflows/post_message_list",
   inputs: {
-    channel_id: ["C05HRRJQ947"],
+    channel_id: { value: "C05HRRJQ947" },
+    interactivity: {
+      value: TriggerContextData.Shortcut.interactivity,
+    },
   },
   schedule: {
     // Schedule the post to happen once every day at 9 AM EST
     //start_time: new Date(new Date().getTime() + 60000).toISOString(),
-    start_time: "2023-07-24T09:00:00Z",
+    start_time: "2023-07-26T02:58:00Z",
     frequency: { type: "hourly", repeats_every: 1 },
   },
 };
